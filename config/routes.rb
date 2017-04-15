564e62f9b3c4e1c503473e10901aac665876e5c7
Frimpong Opoku Agyemang <mrfimpong@gmail.com>
2017-04-15 09:43:07 -0700

    Testing branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
   resources :bdaes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#set my home page to pages#home
+#pages is the name of the controller and home, is a method in it 
+  root to: 'pages#home'
 end
